8b3bb058cfa00453ede3b2c5aa5f446dc78c6a0d
SimonLou-Dev <simsimbidet@gmail.com>
2021-11-09 19:00:12 +0100

    Fix Middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,11 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'http://127.0.0.1:8000/tunnel'
+        'http://127.0.0.1:8000/tunnel',
+        'http://localhost:8000/tunnel',
+        'http://dev-bcfd.simon-lou.com/tunnel',
+        'https://dev-bcfd.simon-lou.com/tunnel',
+        'http://bcfd.simon-lou.com/tunnel',
+        'https://bcfd.simon-lou.com/tunnel'
     ];
 }
