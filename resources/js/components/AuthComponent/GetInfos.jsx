3c9ba6db51c44290d08437d94a35bcf9bc390830
Simon Lou <36455117+SimonLou-Dev@users.noreply.github.com>
2022-03-18 22:45:36 +0100

    Update GetInfos.jsx

--- resources/js/components/AuthComponent/GetInfos.jsx
+++ resources/js/components/AuthComponent/GetInfos.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link, NavLink} from "react-router-dom";
 import axios from "axios";
 import SwitchBtn from "../props/SwitchBtn";
